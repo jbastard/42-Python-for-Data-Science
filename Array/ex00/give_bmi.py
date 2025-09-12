3cdeb5aa44e1a07d65c4a22eddcbd0fd9e0b019d
jbastard <jonas76bastard@gmail.com>
2025-09-12 13:34:00 +0200

    + | Clusters

--- Array/ex00/give_bmi.py
+++ Array/ex00/give_bmi.py
@@ -1,6 +1,7 @@
 def give_bmi(height: list[int | float], weight: list[int | float]) -> list[int | float]:
     return
 
+# bmi = poids[kg] / taille^2[m^2]
 
 def apply_limit(bmi: list[int | float], limit: int) -> list[bool]:
     return
